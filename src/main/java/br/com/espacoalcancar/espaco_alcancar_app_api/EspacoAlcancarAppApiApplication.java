1a11097128d710eec1a32df95057d866f97c18ad
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-09-17 21:35:06 -0300

    feat: adicionando dependencias

---
+++ src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/EspacoAlcancarAppApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.espacoalcancar.espaco_alcancar_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacoAlcancarAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacoAlcancarAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/espacoalcancar/espaco_alcancar_app_api/EspacoAlcancarAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.espacoalcancar.espaco_alcancar_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacoAlcancarAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
