bca09a501c792bda159a68968012e87bae907a83
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-09-19 23:48:58 -0300

    feat: adicionado token em todo o contexto do User

--- src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/security/SecurityConfig.java
+++ src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package br.com.espacoalcancar.espaco_alcancar_app_api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+  @Autowired
+  private SecurityFilter securityFilter;
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,7 +22,8 @@ public class SecurityConfig {
           auth.requestMatchers("/user/new").permitAll()
               .requestMatchers("/auth").permitAll();
           auth.anyRequest().authenticated();
-        });
+        })
+        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
     return http.build();
   }
 
---
+++ src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/security/SecurityFilter.java
@@ -0,0 +1,52 @@
+package br.com.espacoalcancar.espaco_alcancar_app_api.security;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.espacoalcancar.espaco_alcancar_app_api.providers.JWTProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private JWTProvider jwtProvider;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    // Removendo qualquer autorização que exista
+    SecurityContextHolder.getContext().setAuthentication(null);
+
+    String header = request.getHeader("Authorization");
+
+    if (header != null) {
+      var subjectToken = jwtProvider.validateToken(header);
+
+      if (subjectToken.isEmpty()) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return;
+      }
+      request.setAttribute("user_id", subjectToken);
+      UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null,
+          Collections.emptyList());
+      // Abaixo: Usado para que em todas as requisições o spring security possa
+      // validar as informações do usuário
+      SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    filterChain.doFilter(request, response);
+
+  }
+
+}
