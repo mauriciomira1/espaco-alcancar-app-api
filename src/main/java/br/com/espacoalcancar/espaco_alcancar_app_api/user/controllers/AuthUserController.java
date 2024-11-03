8afe638c1d34c2cbd8e336a9c45b66e2f7fa53db
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-11-03 16:32:37 -0300

    feat: add refreshToken no Cookie

--- src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/user/controllers/AuthUserController.java
+++ src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/user/controllers/AuthUserController.java
@@ -1,5 +1,8 @@
 package br.com.espacoalcancar.espaco_alcancar_app_api.user.controllers;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.espacoalcancar.espaco_alcancar_app_api.providers.JWTProvider;
 import br.com.espacoalcancar.espaco_alcancar_app_api.user.models.dto.AuthUserRequest;
 import br.com.espacoalcancar.espaco_alcancar_app_api.user.models.dto.AuthUserResponse;
 import br.com.espacoalcancar.espaco_alcancar_app_api.user.services.AuthUserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class AuthUserController {
@@ -19,12 +26,68 @@ public class AuthUserController {
   @Autowired
   AuthUserService authService;
 
+  @Autowired
+  JWTProvider jwt;
+
   // Login de usuário
   @PostMapping("/auth")
-  public ResponseEntity<Object> create(@RequestBody AuthUserRequest request) throws AuthenticationException {
+  public ResponseEntity<Object> create(@RequestBody AuthUserRequest request, HttpServletResponse response)
+      throws AuthenticationException {
     try {
       AuthUserResponse result = this.authService.execute(request);
+
+      Cookie refreshTokenCookie = new Cookie("refreshToken", result.getRefreshToken());
+      refreshTokenCookie.setHttpOnly(true);
+      refreshTokenCookie.setSecure(true);
+      refreshTokenCookie.setPath("/");
+      refreshTokenCookie.setMaxAge((int) Duration.ofDays(30).getSeconds());
+      response.addCookie(refreshTokenCookie);
+
+      System.err.println("Refresh Token: " + result.getRefreshToken());
       return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+  }
+
+  @PostMapping("/auth/refresh")
+  public ResponseEntity<Object> refresh(HttpServletRequest request, HttpServletResponse response) {
+    try {
+      // Obtendo o refresh token do cookie
+      Cookie[] cookies = request.getCookies();
+      String refreshToken = null;
+      if (cookies != null) {
+        for (Cookie cookie : cookies) {
+          if (cookie.getName().equals("refreshToken")) {
+            refreshToken = cookie.getValue();
+            break;
+          }
+        }
+      }
+
+      if (refreshToken == null) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh token not found.");
+      }
+
+      String userId = this.jwt.validateToken(refreshToken);
+      if (userId == null) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token.");
+      }
+
+      // Gerando novos tokens
+      String newToken = this.jwt.generateToken(Integer.parseInt(userId), Instant.now().plus(Duration.ofHours(2)));
+      String newRefreshToken = this.jwt.generateToken(Integer.parseInt(userId),
+          Instant.now().plus(Duration.ofDays(30)));
+
+      // Atualizando o refresh token no cookie
+      Cookie newRefreshTokenCookie = new Cookie("refreshToken", newRefreshToken);
+      newRefreshTokenCookie.setHttpOnly(true);
+      newRefreshTokenCookie.setSecure(true);
+      newRefreshTokenCookie.setPath("/");
+      newRefreshTokenCookie.setMaxAge((int) Duration.ofDays(30).getSeconds());
+      response.addCookie(newRefreshTokenCookie);
+
+      return ResponseEntity.ok().body(newToken);
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
