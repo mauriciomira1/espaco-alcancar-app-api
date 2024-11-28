5ce5656e4b77665b71e57d8dd539af8c6b70bf46
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-11-28 16:03:58 -0300

    feat: add edição de dados do profissional

--- src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/professional/controllers/ProfessionalController.java
+++ src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/professional/controllers/ProfessionalController.java
@@ -3,14 +3,20 @@ package br.com.espacoalcancar.espaco_alcancar_app_api.professional.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.espacoalcancar.espaco_alcancar_app_api.professional.models.dto.EditProfessionalDTO;
 import br.com.espacoalcancar.espaco_alcancar_app_api.professional.models.dto.NewProfessionalDTO;
+import br.com.espacoalcancar.espaco_alcancar_app_api.professional.models.dto.ProfessionalDashboardResponse;
 import br.com.espacoalcancar.espaco_alcancar_app_api.professional.models.entities.ProfessionalEntity;
 import br.com.espacoalcancar.espaco_alcancar_app_api.professional.services.ProfessionalService;
+import br.com.espacoalcancar.espaco_alcancar_app_api.user.models.ProfileType;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/professional")
@@ -31,6 +37,7 @@ public class ProfessionalController {
   }
 
   // Arquivar um profissional
+  @PutMapping("/archive")
   public ResponseEntity<String> archive(Integer id) {
     try {
       professionalService.archiveProfessional(id);
@@ -41,6 +48,7 @@ public class ProfessionalController {
   }
 
   // Desarquivar um profissional
+  @PutMapping("/unarchive")
   public ResponseEntity<String> unarchive(Integer id) {
     try {
       professionalService.unarchiveProfessional(id);
@@ -51,6 +59,7 @@ public class ProfessionalController {
   }
 
   // Buscar um profissional
+  @GetMapping("/get")
   public ResponseEntity<ProfessionalEntity> get(Integer id) {
     try {
       return ResponseEntity.ok().body(professionalService.getProfessional(id));
@@ -59,13 +68,38 @@ public class ProfessionalController {
     }
   }
 
-  // Atualizar um profissional
-  public ResponseEntity<String> update(Integer id, NewProfessionalDTO request) {
+  // Obter profissional atual
+  @GetMapping("/me")
+  public ResponseEntity<ProfessionalDashboardResponse> getCurrentProfessional() {
     try {
-      professionalService.updateProfessional(id, request);
+      ProfessionalEntity entity = professionalService.getCurrentProfessional();
+
+      ProfessionalDashboardResponse response = new ProfessionalDashboardResponse();
+      response.setId(entity.getId());
+      response.setName(entity.getName());
+      response.setEmail(entity.getEmail());
+      response.setPhone(entity.getPhone());
+      response.setPassword(entity.getPassword());
+      response.setActive(entity.isActive());
+      response.setBirth(entity.getBirth());
+      response.setRegisterNumber(entity.getRegisterNumber());
+      response.setOccupation(entity.getOccupation());
+      response.setProfileType(new ProfileType(false, true, false));
+
+      return ResponseEntity.ok(response);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+  }
+
+  // Atualizar um profissional
+  @PutMapping("/edit")
+  public ResponseEntity<String> update(@Valid @RequestBody EditProfessionalDTO request) {
+    try {
+      professionalService.updateProfessional(request);
       return ResponseEntity.ok().body("Profissional atualizado com sucesso");
     } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao atualizar profissional");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao atualizar profissional: " + e);
     }
   }
 }
