14f1e222df9c22ee54d97e60d893ce320031b591
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-12-01 17:34:11 -0300

    feat: mais chamadas de listagem e handlerException

---
+++ src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/applications/exceptions/UnauthorizedHandlerException.java
@@ -0,0 +1,8 @@
+package br.com.espacoalcancar.espaco_alcancar_app_api.applications.exceptions;
+
+public class UnauthorizedHandlerException extends RuntimeException {
+
+  public UnauthorizedHandlerException(String message) {
+    super(message);
+  }
+}
