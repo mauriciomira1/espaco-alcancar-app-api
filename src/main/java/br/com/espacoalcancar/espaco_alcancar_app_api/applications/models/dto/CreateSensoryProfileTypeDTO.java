5587e347d336f268ff62a7ab5aceffeb02d854aa
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-12-07 19:36:01 -0300

    feat: pequenos ajustes em requisições

--- src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/applications/models/dto/CreateSensoryProfileTypeDTO.java
+++ src/main/java/br/com/espacoalcancar/espaco_alcancar_app_api/applications/models/dto/CreateSensoryProfileTypeDTO.java
@@ -1,8 +1,10 @@
 package br.com.espacoalcancar.espaco_alcancar_app_api.applications.models.dto;
 
+import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 public class CreateSensoryProfileTypeDTO {
-  private String childId;
+  private UUID childId;
 }
