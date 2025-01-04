5763074dbbf748679e1c55cf2df2ba3fb7963464
Maurício Miranda <mauriciomirandagomes@gmail.com>
2025-01-03 21:55:09 -0300

    feat: continuando testes de autenticação de user

--- src/test/java/br/com/espacoalcancar/espaco_alcancar_app_api/user/repositories/UserRepositoryTest.java
+++ src/test/java/br/com/espacoalcancar/espaco_alcancar_app_api/user/repositories/UserRepositoryTest.java
@@ -1,10 +1,23 @@
 package br.com.espacoalcancar.espaco_alcancar_app_api.user.repositories;
 
+import br.com.espacoalcancar.espaco_alcancar_app_api.user.models.dto.UserRequest;
+import br.com.espacoalcancar.espaco_alcancar_app_api.user.models.entities.UserEntity;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 class UserRepositoryTest {
+
+    @Autowired
+    EntityManager entityManager;
 
     @Test
     void findByName() {
     }
+
+    /*private UUID createUser(UserRequest data) {
+        newUser = new UserRequest(data);
+    }*/
 }
